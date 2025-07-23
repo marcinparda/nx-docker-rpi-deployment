0cf8317c1f1e6130cdeba605929037ec1ee4763d
MarcinParda <marcin98parda@gmail.com>
2025-07-23 16:32:36 +0200

    test: preparing change to cancel the deploy

--- libs/app-1-lib/src/lib/app-1-lib.tsx
+++ libs/app-1-lib/src/lib/app-1-lib.tsx
@@ -8,7 +8,7 @@ export function App1Lib(props: App1LibProps) {
   return (
     <div className={styles['container']}>
       <h1>App-1 Specific Component</h1>
-      <h2>Final Flash Kamehameha!</h2>
+      <h2>Kamehameha!</h2>
       <p>{props.message || 'This component is specific to App-1!'}</p>
     </div>
   );
